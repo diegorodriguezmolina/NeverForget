c3fe2b5c1013dbb7ef8774bf186f73f424686319
diegorodriguezmolina <diegorodriguezmolina21@gmail.com>
2018-12-02 20:07:54 +0100

    puzzle

---
+++ Never_Forget/scripts/games/puzzle.js
@@ -0,0 +1,9 @@
+var piezas = document.getElementsByClassName('piezas');
+
+var tamwidth="100";
+var tamheight="100";
+
+for(i=0; i<piezas; i++){
+    piezas[i].style.width = "tamwidth";
+    piezas[i].style.height = "tamheight";
+}

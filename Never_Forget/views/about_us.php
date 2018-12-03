f51fccf35713f210c2ca64968f3918f390a7e886
Francesc Retamal <franrere.6.97@gmail.com>
2018-12-03 10:17:55 +0100

    about us

---
+++ Never_Forget/views/about_us.php
@@ -0,0 +1,90 @@
+<?php include "../templates/master.php" ?>
+
+<?php startblock('head') ?>
+    <title>About us</title>
+    <link rel="stylesheet" href="../Styles/about_us.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<?php endblock() ?>
+<?php startblock('principal') ?>
+    <div class="main">
+        <div class="titulo">
+            <h1>Adventur3</h1>
+        </div>
+
+        <div>
+            <p class="abstract">fyjftyfjj</p>
+        </div>
+
+        <div class="container">
+
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Esther.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Esther Galdeano</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Diego.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Diego Rodriguez</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="../assets/img/avatar/avatar_Francesc.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Francesc Retamal</h5>
+                            <p class="card-text">Pequeña descipción de cada uno</p>
+                            <div class="card-footer justify-content-center">
+                                <ul>
+                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es"><i class="fa fa-twitter iconAbout"></i></a></li>
+                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es"><i class="fa fa-linkedin iconAbout"></i></a></li>
+                                    <li><a href="https://twitter.com/Fran97Chuk?lang=es"><i class="fa fa-instagram iconAbout"></i></a></li>    
+                                </ul>   
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Jordi Parellada</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+
+                            </div>                    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top rounded-circle" src="" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Jordi Peña</h5>
+                            <p class="card-text"></p>
+                            <div class="card-footer justify-content-center">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php endblock() ?>

c4ab0f31eced0a42d074670904bc2624e1382d18
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-07 17:43:40 +0100

    back2

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -16,6 +16,20 @@
 </head>
 <body>
 
+<div id="score">0</div>
+<div id="ship"></div>
+<div id="ui">
+  <div id="uiBox">
+    <div id="text"></div>
+    <br>
+    <div id="uiScore"></div>
+    <br>
+<div id = "highscore">0</div>
+<br>
+    <button id="play"></button>
+  </div>
+</div>
+
 
     
 </body>

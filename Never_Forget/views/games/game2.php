0205f232465c4cd46bbe767f9208e6aadca4e148
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-08 10:46:17 +0100

    new

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -3,34 +3,14 @@
 <head>
 <title>Dodge Game</title>
     <!--STYLES-->
-    <link rel="stylesheet" href="/NeverForget/Never_Forget/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../../Styles/games/game2.css">
-    
-    <!--GAME-->
-    <script type="text/javascript" src="../../scripts/games/game2/game2.js"></script>
 
-    <!--BOOTSTRAP-->
-    <script src="/NeverForget/Never_Forget/js/popper.min.js"></script>
-    <script src="/NeverForget/Never_Forget/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
     
+
 </head>
 <body>
 
-<div id="score">0</div>
-<div id="ship"></div>
-<div id="ui">
-  <div id="uiBox">
-    <div id="text"></div>
-    <br>
-    <div id="uiScore"></div>
-    <br>
-<div id = "highscore">0</div>
-<br>
-    <button id="play"></button>
-  </div>
-</div>
+  <div id="character"></div>
 
-
-    
 </body>
 </html>

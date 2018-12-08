c7428be78d7a842dfaf8c9ff8700fe3c29cb77f3
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-08 10:57:34 +0100

    1

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -5,12 +5,16 @@
     <!--STYLES-->
 
     <link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
+
+    
     
 
 </head>
 <body>
 
-  <div id="character"></div>
+  <div id="character">
+    
+  </div>
 
 </body>
 </html>

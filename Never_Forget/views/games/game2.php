06d2f100fdffe8e837ce4a2a8abc20eb2a05a78e
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-08 14:07:22 +0100

    1

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -2,19 +2,70 @@
 <html>
 <head>   
   
-    <link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-    <script type="text/javascript" src=""></script>
+<title>Memory Game</title>
 
-<title>Dodge Game</title>
-    
+<link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
 
 </head>
 <body>
 
-<p id="score">0</p>
-      <!-- this might be bad form, but it's easier for the javascript to find the position if it's here-->
-      <div id="player" style="left: 50%; top: 50%;"></div>
-      <p id="high_score">high score: 0</p>
+   <section class="memory-game">
+    <div class="memory-card" data-framework="aurelia">
+      <img class="front-face" src="Never_Forget\assets\img\game2\aurelia.svg" alt="Aurelia" />
+      <img class="back-face" src="Never_Forget\assets\img\game2\js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="aurelia">
+      <img class="front-face" src="../img/aurelia.svg" alt="Aurelia" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+
+    <div class="memory-card" data-framework="vue">
+      <img class="front-face" src="../img/vue.svg" alt="Vue" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="vue">
+      <img class="front-face" src="../img/vue.svg" alt="Vue" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+
+    <div class="memory-card" data-framework="angular">
+      <img class="front-face" src="../img/angular.svg" alt="Angular" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="angular">
+      <img class="front-face" src="../img/angular.svg" alt="Angular" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+
+    <div class="memory-card" data-framework="ember">
+      <img class="front-face" src="../img/ember.svg" alt="Ember" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="ember">
+      <img class="front-face" src="../img/ember.svg" alt="Ember" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+
+    <div class="memory-card" data-framework="backbone">
+      <img class="front-face" src="../img/backbone.svg" alt="Backbone" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="backbone">
+      <img class="front-face" src="../img/backbone.svg" alt="Backbone" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+
+    <div class="memory-card" data-framework="react">
+      <img class="front-face" src="../img/react.svg" alt="React" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+    <div class="memory-card" data-framework="react">
+      <img class="front-face" src="../img/react.svg" alt="React" />
+      <img class="back-face" src="../img/js-badge.svg" alt="JS Badge" />
+    </div>
+  </section>
+
+
+
 </body>
 </html>

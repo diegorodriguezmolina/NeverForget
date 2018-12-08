a49c10dad19019f0310f6533474e2c88d86c356b
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-08 12:36:53 +0100

    11

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Dodge Game</title>
-    <!--STYLES-->
-
+<head>   
+  
     <link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src=""></script>
 
-    
+<title>Dodge Game</title>
     
 
 </head>
 <body>
 
-  <div id="character">
-    
-  </div>
-
+<p id="score">0</p>
+      <!-- this might be bad form, but it's easier for the javascript to find the position if it's here-->
+      <div id="player" style="left: 50%; top: 50%;"></div>
+      <p id="high_score">high score: 0</p>
 </body>
 </html>

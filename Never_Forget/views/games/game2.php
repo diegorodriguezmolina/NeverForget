376f9cb3ab7520dc8892080181e4e58498f5eec0
jordipe77 <43988956+jordipe77@users.noreply.github.com>
2018-12-08 18:26:13 +0100

    memory

--- Never_Forget/views/games/game2.php
+++ Never_Forget/views/games/game2.php
@@ -6,66 +6,78 @@
 
 <link rel="stylesheet" type="text/css" href="/NeverForget/Never_Forget/Styles/games/game2.css">
 
-<script type="text/javascript" src="../../scripts/games/game2/game2.js"></script>
+
 
 </head>
 <body>
+<div id="container">
+  <h1>Memory Game</h1>
+  </div>
 
    <section class="memory-game">
-    <div class="memory-card" data-framework="aurelia">
-      <img class="front-face" src="../../assets/img/game2/" alt="Aurelia" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="aurelia">
-      <img class="front-face" src="../../assets/img/game2/" alt="Aurelia" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
 
-    <div class="memory-card" data-framework="vue">
-      <img class="front-face" src="../../assets/img/game2/" alt="Vue" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="vue">
-      <img class="front-face" src="../../assets/img/game2/" alt="Vue" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
+      <div class="memory-card" data-framework="bomb">
+        <img class="front-face" src="../../assets/img/game2/bomb.jpg" alt="Bomb" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="bomb">
+        <img class="front-face" src="../../assets/img/game2/bomb.jpg" alt="Bomb" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
 
-    <div class="memory-card" data-framework="angular">
-      <img class="front-face" src="../../assets/img/game2/" alt="Angular" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="angular">
-      <img class="front-face" src="../../assets/img/game2/" alt="Angular" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
+      <div class="memory-card" data-framework="flag">
+        <img class="front-face" src="../../assets/img/game2/flag.jpg" alt="Flag" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="flag">
+        <img class="front-face" src="../../assets/img/game2/flag.jpg" alt="Flag" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
 
-    <div class="memory-card" data-framework="ember">
-      <img class="front-face" src="../../assets/img/game2/" alt="Ember" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="ember">
-      <img class="front-face" src="../../assets/img/game2/" alt="Ember" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
+      <div class="memory-card" data-framework="helmet">
+        <img class="front-face" src="../../assets/img/game2/helmet.png" alt="Helmet" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="helmet">
+        <img class="front-face" src="../../assets/img/game2/helmet.png" alt="Helmet" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
 
-    <div class="memory-card" data-framework="backbone">
-      <img class="front-face" src="../../assets/img/game2/" alt="Backbone" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="backbone">
-      <img class="front-face" src="../../assets/img/game2/" alt="Backbone" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
+      <div class="memory-card" data-framework="misil">
+        <img class="front-face" src="../../assets/img/game2/misil.jpg" alt="Misil" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="misil">
+        <img class="front-face" src="../../assets/img/game2/misil.jpg" alt="Misil" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
 
-    <div class="memory-card" data-framework="react">
-      <img class="front-face" src=../../assets/img/game2/" alt="React" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
-    <div class="memory-card" data-framework="react">
-      <img class="front-face" src="../../assets/img/game2/" alt="React" />
-      <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="JS Badge" />
-    </div>
+      <div class="memory-card" data-framework="plane">
+        <img class="front-face" src="../../assets/img/game2/plane.jpg" alt="Plane" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="plane">
+        <img class="front-face" src="../../assets/img/game2/plane.jpg" alt="Plane" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+
+      <div class="memory-card" data-framework="tank">
+        <img class="front-face" src="../../assets/img/game2/tank.png" alt="Tank" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
+      <div class="memory-card" data-framework="tank">
+        <img class="front-face" src="../../assets/img/game2/tank.png" alt="Tank" />
+        <img class="back-face" src="../../assets/img/game2/backcard.jpg" alt="backcard" />
+      </div>
   </section>
+  
+  <button>RESTART</button>
+
+  
+  
+
+
+  <script type="text/javascript" src="../../scripts/games/game2/game2.js"></script>
 
 
 

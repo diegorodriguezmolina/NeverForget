b576a1802c4926704aa5a81b7c573601dc38804b
Esther Galdeano <stergaldeano@gmail.com>
2018-12-12 20:20:26 +0100

    Ruta comentada para server

--- Never_Forget/librerias/bd.php
+++ Never_Forget/librerias/bd.php
@@ -49,6 +49,7 @@ function openBD(){
 
      try {
         $conn = new PDO("mysql:host=$servername;dbname=adventur3;cahrset=utf8", $username, $password);
+    // $conn = new PDO("mysql:host=$servername;dbname=daw2a03;cahrset=utf8", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully"; 

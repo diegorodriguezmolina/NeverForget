aead162f1ff4578e5725dc910644dcb185deb2bc
Diego Rodríguez Molina <diegorodriguezmolina21@gmail.com>
2018-12-12 15:02:24 +0100

    next

--- Never_Forget/scripts/games/game4/puzzle.js
+++ Never_Forget/scripts/games/game4/puzzle.js
@@ -1,14 +1,15 @@
 
-//  document.addEventListener("DOMContentLoaded", function(){
-//      shuffle();
-//      var tiempo=setTimeout('saludo()',300000);
-//      finish();
-//  }, false);
+// document.addEventListener("DOMContentLoaded", function(){
+//     shuffle();
+//     var tiempo=setTimeout('help()',5000);
+    
+// }, false);
 
-var intentos=-9;
+var intentos=0
+
 n=0;
 var tiempo = window.setInterval(function(){
-    document.getElementById('tiempo').innerHTML = n;
+    tiempo = document.getElementById('tiempo').innerHTML = n;
     n++;
 },1000);
 
@@ -20,23 +21,10 @@ function swap(tile1, tile2){
     document.getElementById(tile2).className = aux;
     console.log(tile2);
 
-    if(checksolved()==true){
-        
-        document.getElementById("cell33").className= "Complete";
-        //clearInterval('tiempo'); NO FUNCIONA!!
-        document.getElementById("puzzle").style.transition= ".5s ease-in-out";
-        for(var row=1; row<=3; row++){
-            for(var column=1; column<=3; column++){
-                document.getElementById("cell"+row+column).style.border= "none";
-                document.getElementById("cell"+row+column).style.filter= "blur(2px)";
-            }
+    if(checksolved()){
+        if(intentos>1){
+            solved();
         }
-      
-        document.getElementById("puzzle").style.transform= "scale(1.2)";
-        
-        document.getElementById("win").style.display= "block";
-        document.getElementById("winContinue").style.display= "block";
-     
     }
     contador();
     
@@ -52,6 +40,7 @@ function shuffle(){
             swap("cell"+row+column, "cell"+randomRow+randomColumn);
         }
     }
+    intentos=0;
 }
 
 
@@ -60,9 +49,32 @@ function changeTile(row,column){
     var tile= document.getElementById("cell"+row+column);
     
     if (tile.className != "cell9"){
-      
+    //   if(coprobarDosBlancos()){
+        
+        if(column<3 && row<3){
+            if(document.getElementById("cell"+row+(column+1)).className=="cell9" && document.getElementById("cell"+(row+1)+column).className=="cell9" ){
+                if(document.getElementById("cell"+row+(column+1)).click()){
+                    swap("cell"+row+column, "cell"+row+(column+1));
+                }
+                if(document.getElementById("cell"+(row+1)+column).click()){
+                    swap("cell"+row+column, "cell"+(row+1)+column);
+                } 
+            }
+        }
+        if(column>1 && row>1){
+            if(document.getElementById("cell"+row+(column-1)).className=="cell9" && document.getElementById("cell"+(row-1)+column).className=="cell9" ){
+                if(document.getElementById("cell"+row+(column-1)).click()){
+                    swap("cell"+row+column, "cell"+row+(column-1));
+                }
+                if(document.getElementById("cell"+(row-1)+column).click()){
+                    swap("cell"+row+column, "cell"+(row-1)+column);
+                } 
+            }
+        }
+    //   }
+    //   else{
         if(column<3){
-          
+            
             if(document.getElementById("cell"+row+(column+1)).className=="cell9"){
                 swap("cell"+row+column, "cell"+row+(column+1));
             }
@@ -84,24 +96,34 @@ function changeTile(row,column){
             }
         }
     }
+    //   }
     
     document.getElementById('contador').innerHTML = intentos;
     
-
-    finish();
+    if(intentos == 200){
+        solved();
+    }
+    
     
 }
 
+// function coprobarDosBlancos(){
+//     (document.getElementById("cell"+(row+1)+column).className==cell9 && document.getElementById("cell"+row+(column+1).className==cell9) 
+//     || (document.getElementById("cell"+(row-1)+column).className==cell9 && document.getElementById("cell"+row+(column-1)).className==cell9)
+//     || (document.getElementById("cell"+row+(column-1)).className==cell9 && document.getElementById("cell"+row+(column+1)).className==cell9)
+//     || (document.getElementById("cell"+(row+1)+column).className==cell9 && document.getElementById("cell"+(row-1)+column).className==cell9))
+    
+// }
 
-function finish(){
-    if(intentos >20){
-        alert("fin");
-    }
-}
+// function help(){
+//     aux=document.getElementById("cell11").className;
+//     document.getElementById("cell11").className="cell9";
+//     document.getElementById("cellhelp").className=aux;
 
-function saludo()
-{
-    alert("Se ha acabado el tiempo");
+// }
+
+function help(){
+    document.getElementById("help").style.display= "block";
 }
 
 function checksolved(){
@@ -124,5 +146,23 @@ function contador(){
 
     intentos++;
 
+}
+
+function solved() {
+    
+    document.getElementById("cell33").className= "Complete";
+    // clearInterval('tiempo'); !!NO FUNCIONA
+    document.getElementById("puzzle").style.transition= ".5s ease-in-out";
+    for(var row=1; row<=3; row++){
+        for(var column=1; column<=3; column++){
+            document.getElementById("cell"+row+column).style.border= "none";
+            document.getElementById("cell"+row+column).style.filter= "blur(2px)";
+        }
+    }
+    
+    document.getElementById("puzzle").style.transform= "scale(1.2)";
+    
+    document.getElementById("win").style.display= "block";
+    document.getElementById("winContinue").style.display= "block";
 }
 
--- Never_Forget/views/games/game4.php
+++ Never_Forget/views/games/game4.php
@@ -10,41 +10,48 @@
 
 
 <?php startblock('principal') ?>
-    
-        <div id="puzzle"> 
-            <h1 class="text-white" id="win">FELICIDADES</h1>
-            <button id="winContinue">
-                <h1 class="text-white">Continuar la aventura</h1>
-            </button>
-            <div class="row">
-                <div id="cell11" class="cell1" onclick="changeTile(1,1)"></div>
-                <div id="cell12" class="cell2" onclick="changeTile(1,2)"></div>
-                <div id="cell13" class="cell3" onclick="changeTile(1,3)"></div>
+  
+            <div id="puzzle"> 
+                <h1 class="text-white" id="win">FELICIDADES</h1>
+                <button id="winContinue">
+                    <h1 class="text-white">Continuar la aventura</h1>
+                </button>
+                <div class="row">
+                    <div id="cell11" class="cell1" onclick="changeTile(1,1)"></div>
+                    <div id="cell12" class="cell2" onclick="changeTile(1,2)"></div>
+                    <div id="cell13" class="cell3" onclick="changeTile(1,3)"></div>
+                </div>
+                
+                <div class="row">
+                    <div id="cell21" class="cell4" onclick="changeTile(2,1)"></div>
+                    <div id="cell22" class="cell5" onclick="changeTile(2,2)"></div>
+                    <div id="cell23" class="cell6" onclick="changeTile(2,3)"></div>
+                </div>
+                <div class="row">
+                    <div id="cell31" class="cell7" onclick="changeTile(3,1)"></div>
+                    <div id="cell32" class="cell8" onclick="changeTile(3,2)"></div>
+                    <div id="cell33" class="cell9" onclick="changeTile(3,3)"></div>
+                </div>
+            </div> 
+
             
-            </div>
+                <!-- <div>
+                    <img class="help" src="../../assets/img/REFUGI.jpg" alt="">
+                </div> -->
+    -->
+                <div class="helps">
+                    <div class="option">
+                        <p class="text-white ml-2">MOVIMIENTOS</p>
+                        <div id="contador" class=" text-white float-left mt-2 info"></div>
+                    </div>
+                    <div class="option">
+                        <p class="text-white ml-2">TIEMPO</p>
+                        <div id="tiempo" class="float-left text-white  mt-2 info"></div>
+                    </div>
+                </div>
+     
             
-            <div class="row">
-                <div id="cell21" class="cell4" onclick="changeTile(2,1)"></div>
-                <div id="cell22" class="cell5" onclick="changeTile(2,2)"></div>
-                <div id="cell23" class="cell6" onclick="changeTile(2,3)"></div>
-            </div>
-            <div class="row">
-                <div id="cell31" class="cell7" onclick="changeTile(3,1)"></div>
-                <div id="cell32" class="cell8" onclick="changeTile(3,2)"></div>
-                <div id="cell33" class="cell9" onclick="changeTile(3,3)"></div>
-            </div>
-        </div> 
-        
-        <!-- <div class="col-10"> -->
-            <div id="contador" class=" text-white float-left mt-2 info">
-                INTENTOS:
-            </div>
-            <div id="tiempo" class="float-left text-white  mt-2 info">
-                TIEMPO: 
-            </div>
-        <!-- </div> -->
-        
-    <!-- </div> -->
+
     
     
     
